2e75afe00ad357eb1e669f6f19cb5a8df9fd86dc
Hedgemon4 <sethlinc4@gmail.com>
2023-06-09 13:11:35 -0700

    Change directory

--- replacementScript.py
+++ replacementScript.py
@@ -2,6 +2,8 @@ import os
 from os.path import join
 
 # FILE_DIRECTORY = join("..", "..", "..","home", "runner")
+os.chdir('../../')
+
 print("Python File")
 print(os.getcwd())
 files = os.listdir()
