14cf372fc89b667882c79e71b981606f0e5598d5
Hedgemon4 <sethlinc4@gmail.com>
2023-06-09 12:28:37 -0700

    Added imports and replace for variant files

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-# from os.path import join, isfile
+from os.path import join, isfile
 
 Import("env")
 
@@ -11,8 +11,14 @@ print(str(FRAMEWORK_DIR))
 print(str(WORKING_DIRECTORY))
 print("goodbye")
 
-variant_c_file = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.cpp")
-variant_h_file = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.h")
+variant_c_file_old = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.cpp")
+variant_h_file_old = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.h")
 
-os.remove(variant_c_file)
-os.remove(variant_h_file)
+variant_c_file_new = join(WORKING_DIRECTORY, "variant", "variant.cpp")
+variant_h_file_new = join(WORKING_DIRECTORY, "variant", "variant.h")
+
+os.remove(variant_c_file_old)
+os.remove(variant_h_file_old)
+
+os.replace(variant_c_file_old, variant_c_file_new)
+os.replace(variant_h_file_old, variant_h_file_new)
