496869745fd55c5a4798688e3eb159197b3b1567
Hedgemon4 <sethlinc4@gmail.com>
2023-06-09 12:24:26 -0700

    Changed file to remove variant ifles

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-from os.path import join, isfile
+import os
+# from os.path import join, isfile
 
 Import("env")
 
 FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduino-samd-adafruit")
+WORKING_DIRECTORY = os.getcwd()
+
 print("Hello")
 print(str(FRAMEWORK_DIR))
+print(str(WORKING_DIRECTORY))
 print("goodbye")
+
+variant_c_file = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.cpp")
+variant_h_file = join(FRAMEWORK_DIR, "variants", "feather_m0", "variant.h")
+
+os.remove(variant_c_file)
+os.remove(variant_h_file)
