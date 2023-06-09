4905a5e5cdb4c8c6bd0bf96446190a503bd0fb50
Hedgemon4 <sethlinc4@gmail.com>
2023-06-09 13:30:06 -0700

    Built python script

--- replacementScript.py
+++ replacementScript.py
@@ -1,9 +1,25 @@
 import os
 from os.path import join
 
+print("Python Start")
+
+print(os.getcwd())
+
 os.chdir("/home/runner")
 
 FRAMEWORK_DIR = join(".platformio", "packages", "framework-arduino-samd-adafruit", "variants", "feather_m0")
+VARIANT_DIR = join("work", "iondb", "iondb", "variant")
 
 variant_c_file_old = join(FRAMEWORK_DIR, "variant.cpp")
 variant_h_file_old = join(FRAMEWORK_DIR, "variant.h")
+
+os.remove(variant_c_file_old)
+os.remove(variant_h_file_old)
+
+variant_c_file_new = join(VARIANT_DIR, "variant.cpp")
+variant_h_file_new = join(VARIANT_DIR, "variant.h")
+
+os.repalce(variant_c_file_new, variant_c_file_old)
+os.repalce(variant_h_file_new, variant_h_file_old)
+
+print("Python End")
