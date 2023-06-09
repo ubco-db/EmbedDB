82200487e371fbacea2c30759fcd2ea3145f2270
Hedgemon4 <sethlinc4@gmail.com>
2023-06-09 13:19:09 -0700

    Change file path again

--- replacementScript.py
+++ replacementScript.py
@@ -2,7 +2,8 @@ import os
 from os.path import join
 
 # FILE_DIRECTORY = join("..", "..", "..","home", "runner")
-os.chdir('../../../')
+# os.chdir('../../../')
+os.chdir("/home/runner")
 
 print("Python File")
 print(os.getcwd())
